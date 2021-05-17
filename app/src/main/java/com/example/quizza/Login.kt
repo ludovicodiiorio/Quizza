dfdc1b894da3c8a26bf0c1634f4a9b285a8f2983
AmineMatqoune <aminematqoune@gmail.com>
2021-05-17 20:42:04 +0200

    First two dialogs inserted

--- app/src/main/java/com/example/quizza/Login.kt
+++ app/src/main/java/com/example/quizza/Login.kt
@@ -1,6 +1,8 @@
 package com.example.quizza
 
+import android.content.DialogInterface
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.quizza.databinding.LoginRegisterBinding
 
@@ -19,6 +21,22 @@ class Login : AppCompatActivity(){
 
         loginBinding.buttonRegister.setOnClickListener{
 
+        }
+
+        loginBinding.btnMusic.setOnClickListener{
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle(R.string.exit_dialog_title)
+            builder.setMessage(R.string.exit_dialog_text)
+            builder.setPositiveButton("YES", DialogInterface.OnClickListener{ dialog, it -> println(getString(R.string.yes_text)) })
+            builder.setNegativeButton("NO", DialogInterface.OnClickListener{ dialog, it -> println(getString(R.string.no_text)) })
+            builder.create().show()
+        }
+
+        loginBinding.btnPalette.setOnClickListener{
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Choose a theme")
+            builder.setItems(arrayOf("Dark", "Light", "Neutral"), DialogInterface.OnClickListener{ dialog, which -> println("Hai selezionato " + which.toString())})
+            builder.create().show()
         }
     }
 
